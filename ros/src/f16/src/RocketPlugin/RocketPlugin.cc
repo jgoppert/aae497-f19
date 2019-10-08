eab5381e16fd4746d1f4ef23120503d76a923fb4
James Goppert <james.goppert@gmail.com>
2019-10-08 06:00:20 -0400

    Lower initial start mass.

--- ros/src/f16/src/RocketPlugin/RocketPlugin.cc
+++ ros/src/f16/src/RocketPlugin/RocketPlugin.cc
@@ -102,7 +102,7 @@ void RocketPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
 
   // Disable gravity, we will handle this in plugin
   this->body->SetGravityMode(false);
-  this->body->GetInertial()->SetMass(1.0); //  set your total mass with fuel here
+  this->body->GetInertial()->SetMass(0.4); //  set your total mass with fuel here
 
 
   // Update time.
