3bd7db03b52d63c787e5b8520e358fbd4ddf0e2d
Sidharth-Prasad <sidharth@bhatni.com>
2019-11-06 21:54:21 -0500

    Added build folder

---
+++ homework/hw7/build/CMakeFiles/quadtree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quadtree.dir/quadtree.cpp.o"
+  "quadtree.pdb"
+  "quadtree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quadtree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
